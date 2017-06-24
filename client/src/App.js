58fdf357ceb8c533d6b7019cdabe896b7bd55010
arthurp1 <arthur.poot1@gmail.com>
2017-06-24 13:55:17 +0200

    React Bootstrap css included

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,8 +12,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          AI hackathon
         </p>
+        <Button bsStyle="primary">Primary</Button>
       </div>
     );
   }
