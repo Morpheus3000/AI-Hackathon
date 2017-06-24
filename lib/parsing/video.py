0563b4ac032a019fae5e26ab1f116a3530cf0cb9
Ray Ohm <Ray Ohm>
2017-06-24 12:48:11 +0200

    folders

---
+++ lib/parsing/video.py
@@ -0,0 +1,8 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import yaml
+from matplotlib.pyplot import imshow
+
+
+import parsing.video as video_parsing
---
+++ main.py
@@ -0,0 +1,11 @@
+from lib.speech_to_text import speech_to_text
+import lib.parsing as parsing
+
+
+def main(_):
+
+  speech_to_text.main()
+
+
+if __name__ == '__main__':
+  main()
