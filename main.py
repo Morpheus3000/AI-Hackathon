6fbf98ecba7620cb0cc4b4c3fff7ddc868c80d29
Sergey Pastukhov <sergey.pastukhov@booking.com>
2017-06-25 14:25:48 +0200

    ASD

--- main.py
+++ main.py
@@ -14,31 +14,31 @@ def main():
   # video_file =
   # 'Demo of Online Meeting via Zoom with Participants from Around the World-261OCjeg9GI.mp4'
   # audio_file = video_file + '.wav'
-  with open('api_keys/ray.yml', 'r') as f:
+  with open('../../api_keys.yml', 'r') as f:
     keys = yaml.load(f)
 
-  conference_video = 'Data/TestAdam.mp4'
+  conference_video = 'Data/Adam.mp4'
   cap = video.parse(conference_video)
 
   frame_count = video.frames(cap)
   wavs_dir = 'Data/wavs'
   duration = video.duration(cap)
-  if not os.path.exists(wavs_dir):
-    audio_files = video.audio(conference_video, duration)
-  else:
-    audio_files = [f for f in os.listdir(
-        wavs_dir) if os.path.isfile(os.path.join(wavs_dir, f))]
+  # if not os.path.exists(wavs_dir):
+  # audio_files = video.audio(conference_video, duration)
+  # else:
+  audio_files = [f for f in os.listdir(
+    wavs_dir) if os.path.isfile(os.path.join(wavs_dir, f))]
 
   print(audio_files)
 
-  demo_path = 'lib/visualization/TestAdam_Demo.csv'
+  demo_path = 'lib/visualization/Demo_Adam.csv'
   if not os.path.exists(demo_path):
     speech_data = speech.parse(audio_files, keys)
     speech_data.to_csv(demo_path)
   else:
     speech_data = pd.read_csv(demo_path)
 
-  score_path = 'lib/visualization/TestAdam_Score.csv'
+  score_path = 'lib/visualization/Score_Adam.csv'
   if not os.path.exists(score_path):
     speech_data = update.text.data_frame(speech_data)
     speech_data.to_csv(score_path)
